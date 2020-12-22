246cb71335700fcf947e667598e42434b154ee12
Demqq <73760785+Demqq@users.noreply.github.com>
2020-12-22 18:19:55 +0100

    1

---
+++ cw1/js/task2.js
@@ -0,0 +1,2 @@
+var question = prompt('Добрый день, укажите Ваше имя')
+document.write('Спасибо, что ввели ваше имя, ' + question)
---
+++ cw1/js/task3.js
@@ -0,0 +1,3 @@
+var planet = 'Earth';
+var question = prompt('do you live on ' + planet + '?')
+document.write('Congratulation. Ilon Mask wants you to move to the Mars')
