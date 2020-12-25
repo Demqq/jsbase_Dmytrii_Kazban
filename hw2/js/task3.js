c496d4a792be3934c697ae39a2dfb5e45faf460d
Demqq <73760785+Demqq@users.noreply.github.com>
2020-12-25 20:50:06 +0100

    1

---
+++ hw2/js/task3.js
@@ -0,0 +1,13 @@
+// 3. Определить какое из трех, введенных пользователем, чисел максимальное и вывести его на экран.
+
+var number1 = +prompt ("введите первое число")
+var number2 = +prompt ("введите второе число")
+var number3 = +prompt ("введите третье число")
+
+if (number1 > number2 && number1 > number3) {
+    console.log("число " + number1 + " больше чем остальные, вами введенные числа");
+} else if( number2 > number1 && number2 > number3) {
+    console.log("число " + number2 + " больше чем остальные, вами введенные числа");
+} else if( number3 > number1 && number3 > number2) {
+    console.log("число " + number3 + " больше чем остальные, вами введенные числа");
+}
