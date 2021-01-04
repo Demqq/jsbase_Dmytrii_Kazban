456a86cf64107155590db49755cfe7f74137619b
Demqq <73760785+Demqq@users.noreply.github.com>
2021-01-04 02:41:50 +0100

    3

--- hw3/js/task3.js
+++ hw3/js/task3.js
@@ -1,17 +1,23 @@
 // 3. Создать многомерный массив, размер массива получить от пользователя. Каждый нечётный подмассив заполнить значением 'odd', а чётный - 'even'. 
 //     Размер каждого вложенного массива - 3.
 
-var a = [];
-a.length = +prompt("Введите длину массива");
+
+var b = +prompt("Введите длину массива")
+var a = new Array(b);
+
 console.log(a.length);
 
-for (i = 0; i < a.length; i++){
+for(i = 0; i < a.length; i++){
     a[i] = new Array(3);
 
-    if (a.length % 2 === 0) {
-        a[i] = ["even"];
-    } else {
-        a[i] = ["odd"];
+    for(j = 0; j < 3 ; j++){
+
+        if (i % 2 === 0) {
+            a[i][j] = ["even"];
+        } else {
+            a[i][j] = ["odd"];
+        }
     }
 }
-console.log(a);
+console.log (a);
+
