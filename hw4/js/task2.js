37ddf8e147bd0dc888494acd7c7db5d427cef995
Demqq <73760785+Demqq@users.noreply.github.com>
2021-01-05 22:33:47 +0100

    12

--- hw4/js/task2.js
+++ hw4/js/task2.js
@@ -1,3 +1,17 @@
 // 2.	Создать функцию, возводящую число в степень, число и сама степень вводится пользователем
 // 		Проверить числа на корректность
     
+let numer = +prompt("Poproszę wpisać dowolną liczbę");
+let degree = +prompt("Poproszę wpisać stopeń liczby, ktory Państwo chcą użyć");
+
+function makeDegree(a,b){
+    
+    if(Number.isNaN(a) || Number.isNaN(b)){
+        console.log("to, co Państwo wspisali, nie jest liczbą");
+    }
+
+    var result = Math.pow(a, b);
+    console.log("liczba, podniesiona do potęgi, to : " + result);
+}
+
+makeDegree(numer, degree);
