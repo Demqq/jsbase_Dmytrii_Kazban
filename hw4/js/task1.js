b3998448dd312af00b0051885d389603a0ebbd71
Demqq <73760785+Demqq@users.noreply.github.com>
2021-01-05 15:28:51 +0100

    1

--- hw4/js/task1.js
+++ hw4/js/task1.js
@@ -3,40 +3,41 @@
 // 		Функция должна проверять корректность введенных чисел и знака операции.
 // 		Все аргументы для функции принять от пользователя.
 
-function calculate(a, b, sign){
+function calculate(a, b, sign) {
     var a = +prompt("Enter a first number ");
     var b = +prompt("Enter a second number ");
     var sign = prompt("Enter a arithmetic operation sign +  - * / ");
     var result;
 
-    if (Number.isNaN(a) || Number.isNaN(b)){
+    if (Number.isNaN(a) || Number.isNaN(b)) {
         console.log("The number that you have entered is not really a number")
     }
 
-    if (sign === "+" || "-" || "*" || "/"){
+    if (sign === "+" || "-" || "*" || "/") {
         console.log("You have wrote the wrong arithmetic sign")
     }
 
-    switch(sign){
+    switch (sign) {
         case "+":
             result = a + b;
             break;
-        
+
         case "-":
             result = a - b;
             break;
-        
+
         case "*":
             result = a * b;
             break;
-        
+
         case "/":
             result = a / b;
             break;
-    }   
+    }
 
-    if ( result !== undefined){
+    if (result !== undefined) {
         console.log("The result of you arithmetic operation is: " + result);
     }
 }
+
 calculate();
